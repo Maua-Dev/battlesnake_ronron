4ad2687121598beff75efdef6d6e3fb83447a63e
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2023-07-26 13:41:05 -0300

    add logic 1.0

--- src/app/main.py
+++ src/app/main.py
@@ -4,16 +4,30 @@ from mangum import Mangum
 
 app = FastAPI()
 
+moviments = {
+    0: "up", # y+1
+    1: "down", # y-1
+    2: "left", # x-1
+    3: "right" # x+1
+}
+funcMoviments = {
+    0: lambda head: {'x': head['x'],'y': head['y']+1}, # y+1
+    1: lambda head: {'x': head['x'],'y': head['y']-1}, # y-1
+    2: lambda head: {'x': head['x']-1,'y': head['y']}, # x-1
+    3: lambda head: {'x': head['x']+1,'y': head['y']} # x+1
+}
 # TODO: Implement my logic here to handle the requests from Battlesnake
+def body(position,request):
+  if (position in request['you']['body']):
+    return True
+  return False
 
-def move():
-    moviments = {
-        0: "up",
-        1: "down",
-        2: "left",
-        3: "right"
-    }
-    return moviments[randint(0, 3)]
+def move(request: dict):
+  moviment = moviments[randint(0, 3)]
+  newHead = funcMoviments[moviment](request['you']['head'])
+  while body(newHead,request):
+    moviment = moviments[randint(0, 3)]
+  return moviment
 
 
 @app.get("/")
@@ -35,7 +49,7 @@ def read_item(request: dict, status_code=200):
 @app.post("/move")
 def create_item(request: dict, status_code=200):
     return {
-      "move": move(),
+      "move": move(request),
       "shout": "THIS IS SPARTA!"
     }
    
