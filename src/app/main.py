3bd3580265b1e11ccd8775fb55377b7816efe513
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2023-07-26 14:24:39 -0300

    test

--- src/app/main.py
+++ src/app/main.py
@@ -4,19 +4,13 @@ from mangum import Mangum
 
 app = FastAPI()
 
-moviments = {
-    0: "up", # y+1
-    1: "down", # y-1
-    2: "left", # x-1
-    3: "right" # x+1
-}
 funcMoviments = {
     "up": lambda head: {'x': head['x'],'y': head['y']+1}, # y+1
     "down": lambda head: {'x': head['x'],'y': head['y']-1}, # y-1
     "left": lambda head: {'x': head['x']-1,'y': head['y']}, # x-1
     "right": lambda head: {'x': head['x']+1,'y': head['y']} # x+1
 }
-# TODO: Implement my logic here to handle the requests from Battlesnake
+
 def body(position,request):
   if (position in request['you']['body']):
     return True
@@ -26,14 +20,26 @@ def wall(position):
   if (position['x'] < 0 or position['x'] > 10 or position['y'] < 0 or position['y'] > 10):
     return True
   return False
+def enemiesBody(position,request):
+  if (position in request['board']['snakes']['body'].pop()):
+    return True
+  return False
 
 def move(request: dict):
-  moviment = moviments[randint(0, 3)]
-  newHead = funcMoviments[moviment](request['you']['head'])
-  while body(newHead,request) or wall(newHead):
-    moviment = moviments[randint(0, 3)]
-    newHead = funcMoviments[moviment](request['you']['head'])
-  return moviment
+  moviments = {
+    0: "up", # y+1
+    1: "down", # y-1
+    2: "left", # x-1
+    3: "right" # x+1
+  }
+  move = moviments.pop(randint(0, 3), None)
+  newHead = funcMoviments[move](request['you']['head'])
+  while len(moviments.keys) > 0 and (body(newHead,request) or wall(newHead) or enemiesBody(newHead,request)):
+    move = moviments.pop(randint(0, len(moviments.keys)-1), None)
+    if(move is None):
+      move = 'up'
+    newHead = funcMoviments[move](request['you']['head'])
+  return move
 
 
 @app.get("/")
